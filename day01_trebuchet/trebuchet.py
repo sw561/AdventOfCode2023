849198c130d37c220155c39ff0a294701e061b0c
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2023-12-01 16:31:40 +0000

    Day 1

--- day01_trebuchet/trebuchet.py
+++ day01_trebuchet/trebuchet.py
@@ -17,12 +17,15 @@ def first_last(it):
         last = i
     return first, last
 
-def part1(line):
-    f, l = first_last(x for x in line if x in string.digits)
-    return 10 * int(f) + int(l)
+def calibration_value(digit_iterator):
+    first, last = first_last(digit_iterator)
+    return 10*first + last
 
 def solve_part1(data):
-    return sum(part1(line) for line in data)
+    return sum(
+        calibration_value(int(s) for s in line if s in string.digits)
+            for line in data
+        )
 
 written_digits = {1: "one",
     2: "two",
@@ -34,39 +37,31 @@ written_digits = {1: "one",
     8: "eight",
     9: "nine"}
 
-def repair(s):
+def get_digits(s):
 
-    i = 0
-    while i < len(s):
-        for d, name in written_digits.items():
-            if s[i:].startswith(name):
-                s = s[:i] + str(d) + s[i+len(name):]
-        i += 1
+    for i in range(len(s)):
 
-    return s
+        if s[i] in string.digits:
+            yield int(s[i])
+
+        else:
+            for k, v in written_digits.items():
+                if s[i:].startswith(v):
+                    yield k
 
 
 def solve_part2(data):
-    rep = map(repair, data)
-
-    r = list(rep)
-    for i in range(len(data)):
-        print(f"{data[i]:40s}, {r[i]:30s}, {part1(r[i]):3d}")
-        # print("part1(r[i]):", part1(r[i]))
-
-        # import pdb; pdb.set_trace()
-
-    return solve_part1(r)
+    return sum(
+        calibration_value(get_digits(line))
+            for line in data
+        )
 
 def main():
     data = read("day01_trebuchet/input.txt")
     # data = read("day01_trebuchet/example")
     # data = read("day01_trebuchet/example2")
 
-    # print(data)
-
-    # return solve_part1(data), solve_part2(data)
-    return None, solve_part2(data)
+    return solve_part1(data), solve_part2(data)
 
 
 if __name__=="__main__":
