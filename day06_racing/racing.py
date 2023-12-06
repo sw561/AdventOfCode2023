a2535ca5438d920d0dd3428ab133bf7576d8c8c7
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2023-12-06 21:41:14 +0000

    Runs

--- day06_racing/racing.py
+++ day06_racing/racing.py
@@ -2,36 +2,52 @@
 
 from functools import reduce
 import operator
+from math import sqrt, floor, ceil
 
 def parse(line):
     rest = line.split(':')[1]
-    return [int(x) for x in rest.split()]
+    return [int(x) for x in rest.split()], [int(rest.replace(' ',''))]
 
 def read(fname):
     with open(fname, 'r') as f:
         times, distances = [parse(line) for line in f.read().strip().split('\n')]
-    return times, distances
+    return zip(times, distances)
 
 def distance(hold_time, time):
     return hold_time * (time - hold_time)
 
-def winning_ways(data):
-    for t, d in zip(*data):
-        yield sum(1 for h in range(t) if distance(h, t) > d)
+def brute_winning_ways(t, d):
+    brute_min = min(h for h in range(t) if distance(h, t) > d)
+    brute_max = max(h for h in range(t) if distance(h, t) > d)
+    return brute_max - brute_min + 1
 
-def solve_part1(data):
-    return reduce(operator.mul, winning_ways(data))
+def winning_ways(t, d):
+    # To get winning ways need to find the critical values...
+    #
+    #   x (t - x) = d
+    #
+    #   x^2 - tx + d = 0
+    #
+    #   x = t/2 +- sqrt(t**2/4 - d)
 
-def solve_part2(data):
-    return 0
+    r1 = t/2 - sqrt(t**2/4 - d)
+    r2 = t/2 + sqrt(t**2/4 - d)
+
+    my_min = floor(r1) + 1
+    my_max = ceil(r2) - 1
+
+    return my_max - my_min + 1
+
+def solve(data):
+    return reduce(operator.mul,
+        (winning_ways(t, d) for t, d in zip(*data))
+        )
 
 def main():
-    data = read("day06_racing/input.txt")
-    # data = read("day06_racing/example")
+    part1, part2 = read("day06_racing/input.txt")
+    # part1, part2 = read("day06_racing/example")
 
-    print(data)
-
-    return solve_part1(data), solve_part2(data)
+    return solve(part1), solve(part2)
 
 
 if __name__=="__main__":
--- day06_racing/test_racing.py
+++ day06_racing/test_racing.py
@@ -1,14 +1,16 @@
 import pytest
-from racing import read, winning_ways, solve_part2, main
+from racing import read, winning_ways, brute_winning_ways, main
 
-data = read("day06_racing/example")
+part1, part2 = read("day06_racing/example")
 
 def test_part1():
-    assert list(winning_ways(data)) == [4, 8, 9]
+    l_fast = [winning_ways(t, d) for t, d in zip(*part1)]
+    l_brute = [brute_winning_ways(t, d) for t, d in zip(*part1)]
+    assert l_fast == l_brute == [4, 8, 9]
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2():
-    assert solve_part2()
+    for t, d in zip(*part2):
+        assert winning_ways(t, d) == 71503
 
 @pytest.mark.real
 def test_main():
