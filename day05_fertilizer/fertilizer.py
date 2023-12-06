bf44c18e97d102b045b0c20006973a2592e9d7a6
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2023-12-06 01:24:07 +0000

    Day 5 passing

--- day05_fertilizer/fertilizer.py
+++ day05_fertilizer/fertilizer.py
@@ -4,9 +4,7 @@ def read(fname):
     with open(fname, 'r') as f:
         inp = f.read().strip()
 
-
     maps = []
-
 
     for section in inp.split('\n\n'):
         lines = section.split('\n')
@@ -21,16 +19,70 @@ def read(fname):
     return seeds, maps
 
 def translate(x, stage):
-    # stage is a series of rules, each rule is [destination, source, range length]
+    # stage is a series of rules, each rule is [destination, source, range
+    # length]
     for (d, s, l) in stage:
         if x in range(s, s+l):
             return d + x - s
     return x
 
+def translate_range(r, stage, start_index=0, indent=0):
+
+    # Range includes start, doesn't include end
+    start, end = r
+    # print("stage:", stage)
+    if start >= end:
+        return
+
+    # print("-"*indent, "start, end:", start, end)
+
+    for index in range(start_index, len(stage)):
+        (d, s, l) = stage[index]
+        # print("-"*indent, "d, s, l:", d, s, l)
+        # Map overlapping portion of r with range(s, s+l)
+
+        if end <= s or start >= s+l:
+            # No overlap
+            continue
+
+        o_start = max(s, start)
+        o_end = min(s+l, end)
+
+        yield (d + o_start - s, d + o_end -s)
+
+        yield from translate_range(
+            (start, o_start), stage, index+1, indent=indent+1
+            )
+
+        yield from translate_range(
+            (o_end, end), stage, index+1, indent=indent+1
+            )
+
+        return
+
+    yield (start, end)
+
+def translate_ranges(rs, stage):
+    # Translate a range, and yield
+    for r in rs:
+        yield from translate_range(r, stage)
+
+def translate_ranges_all(rs, stages):
+
+    if len(stages) == 1:
+        yield from translate_ranges(rs, stages[0])
+
+    else:
+        yield from translate_ranges_all(
+            translate_ranges(rs, stages[0]),
+            stages[1:]
+            )
+
 def location(seed, maps):
     x = seed
     for stage in maps:
         x = translate(x, stage)
+        # print("x:", x)
     location = x
     # print("seed, location:", seed, location)
     return location
@@ -40,8 +92,35 @@ def solve_part1(data):
 
     return min(location(seed, maps) for seed in seeds)
 
+def solve_part2_brute_gen(data):
+    seeds, maps = data
+
+    for si in range(0, len(seeds), 2):
+
+        r = (seeds[si], seeds[si]+seeds[si+1])
+
+        for s in range(*r):
+            yield location(s, maps)
+
+def solve_part2_brute(data):
+    return min(solve_part2_brute_gen(data))
+
+
+
+def solve_part2_ranges(data):
+    seeds, maps = data
+
+    for si in range(0, len(seeds), 2):
+
+        r = (seeds[si], seeds[si]+seeds[si+1])
+
+        yield from translate_ranges_all([r], maps)
+
 def solve_part2(data):
-    return 0
+
+    return min(r[0] for r in solve_part2_ranges(data))
+
+
 
 def main():
     data = read("day05_fertilizer/input.txt")
--- day05_fertilizer/test_fertilizer.py
+++ day05_fertilizer/test_fertilizer.py
@@ -1,5 +1,5 @@
 import pytest
-from fertilizer import read, location, solve_part1, solve_part2, main
+from fertilizer import *
 
 data = read("day05_fertilizer/example")
 
@@ -11,9 +11,47 @@ def test_locations():
 def test_part1():
     assert solve_part1(data) == 35
 
-@pytest.mark.skip(reason="Not implemented")
+def test_part2_small_ranges():
+    _, maps = data
+    for seed in range(100):
+        r = (seed, seed+1)
+        l = location(seed, maps)
+        count = 0
+        r_locs = list(translate_ranges_all([r], maps))
+        assert r_locs == [(l, l+1)]
+
+def test_part2_big_range():
+    _, maps = data
+    ls = set()
+    for seed in range(100):
+        ls.add(location(seed, maps))
+
+    ls_comp = set()
+    for r_locs in translate_ranges_all([(0, 100)], maps):
+        print("r_locs:", r_locs)
+        for s in range(*r_locs):
+            ls_comp.add(s)
+
+    assert ls == ls_comp
+
+def test_part2_brute():
+    assert solve_part2_brute(data) == 46
+
 def test_part2():
-    assert solve_part2()
+    locs = set(solve_part2_brute_gen(data))
+
+    locs_comp = set()
+    for r in solve_part2_ranges(data):
+        for s in range(*r):
+            locs_comp.add(s)
+
+    assert locs == locs_comp
+
+    assert min(locs) == 46
+
+    assert solve_part2(data) == 46
+
+
 
 @pytest.mark.real
 def test_main():
