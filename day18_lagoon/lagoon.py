8c21c27739d979c18ec61de11d75852827337a95
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2023-12-18 14:30:33 +0000

    Day 18 part 2

--- day18_lagoon/lagoon.py
+++ day18_lagoon/lagoon.py
@@ -2,7 +2,7 @@
 
 from collections import deque
 
-def read(fname):
+def read(fname, towards_interior):
     data = []
     with open(fname, 'r') as f:
         for line in f.read().strip().split('\n'):
@@ -10,10 +10,10 @@ def read(fname):
             b = int(b)
             c = c.strip('()#')
             data.append((a, b, c))
-    return data
+    return data, towards_interior
 
 
-def display(state, sideA, sideB):
+def display(state, interior):
     imin = min(p[0] for p in state) - 1
     imax = max(p[0] for p in state) + 1
     jmin = min(p[1] for p in state) - 1
@@ -23,10 +23,8 @@ def display(state, sideA, sideB):
         for j in range(jmin, jmax+1):
             if (i, j) in state:
                 print('#', end='')
-            elif (i, j) in sideA:
+            elif (i, j) in interior:
                 print('X', end='')
-            elif (i, j) in sideB:
-                print('Y', end='')
             else:
                 print('.', end='')
         print()
@@ -36,6 +34,7 @@ dxdy = {"L": (0, -1), "R": (0, 1), "U": (-1, 0), "D": (1, 0)}
 d_inverse = {"L": "R", "R": "L", "U": "D", "D": "U"}
 
 def solve_part1(data):
+    data, towards_interior = data
 
     pos = (0, 0)
     state = set()
@@ -48,51 +47,97 @@ def solve_part1(data):
     assert pos == (0, 0)
 
     # Now do bfs to fill the interior
-    sideA = set()
-    dA = deque()
-    sideB = set()
-    dB = deque()
-    # Take one step in direction of first and last instruction to get an interior position
-    for z in [0, 1]:
-        pos = (0, 0)
-        for i in [0, -1]:
-            d, _, _ = data[i]
-            if z == 0 and i == -1:
-                # Reverse direction of last step
-                d = d_inverse[d]
-            pos = tuple(ai + bi for ai, bi in zip(pos, dxdy[d]))
-        if z == 0:
-            sideA.add(pos)
-            dA.append(pos)
-        else:
-            sideB.add(pos)
-            dB.append(pos)
+    s = set()
+    # display(state, s)
 
-    while dA and dB:
-    # for _ in range(100):
-        for d, s in zip([dA, dB], [sideA, sideB]):
+    deq = deque()
+    for d in towards_interior:
+        pos = tuple(ai + bi for ai, bi in zip(pos, dxdy[d]))
 
-            pos = d.popleft()
+    s.add(pos)
+    deq.append(pos)
 
-            for dxy in dxdy.values():
-                new_pos = tuple(ai + bi for ai, bi in zip(pos, dxy))
+    # display(state, s)
 
-                if new_pos in s or new_pos in state:
-                    continue
+    while deq:
+        pos = deq.popleft()
 
-                s.add(new_pos)
-                d.append(new_pos)
+        for dxy in dxdy.values():
+            new_pos = tuple(ai + bi for ai, bi in zip(pos, dxy))
 
-    return len(state) + (len(sideB) if dA else len(sideA))
+            if new_pos in s or new_pos in state:
+                continue
+
+            s.add(new_pos)
+            deq.append(new_pos)
+
+    # display(state, s)
+
+    # print("len(state):", len(state))
+    # print("len(s):", len(s))
+
+    return len(state) + len(s)
+
+def solve_part1_clever(data):
+    data, towards_interior = data
+
+    pos = (0, 0)
+    perimeter = 0
+    enclosed_area = 0 # by shoelace formula
+
+    for d, n, _ in data:
+
+        pos = tuple(ai + n * bi for ai, bi in zip(pos, dxdy[d]))
+
+        perimeter += n
+        if d == "L":
+            enclosed_area -= pos[0] * n
+        elif d == "R":
+            enclosed_area += pos[0] * n
+
+
+        # print("pos:", pos)
+        # print("enclosed_area:", enclosed_area)
+
+
+
+    # print("perimeter:", perimeter)
+    # print("enclosed_area:", enclosed_area)
+
+    area = abs(enclosed_area) + perimeter // 2 + 1
+
+    return area
+
+direction = {'0': "R", '1': "D", '2': "L", '3': "U"}
 
 def solve_part2(data):
-    return 0
+    data, towards_interior = data
+
+    new_data = []
+    for _, _, x in data:
+        distance = int(x[:5], 16)
+        d = direction[x[5]]
+        new_data.append((d, distance, x))
+
+    # for d, i, _ in new_data:
+    #     print("d, i:", d, i)
+
+    return solve_part1_clever((new_data, towards_interior))
 
 def main():
-    data = read("day18_lagoon/input.txt")
-    # data = read("day18_lagoon/example")
+    data = read("day18_lagoon/input.txt", "DR")
+    # data = read("day18_lagoon/example", "DR")
+    # data = read("day18_lagoon/example2", "DR")
 
-    return solve_part1(data), solve_part2(data)
+    # p1 = solve_part1(data)
+
+    p1c = solve_part1_clever(data)
+
+    p2 = solve_part2(data)
+
+    print("p2:", p2)
+
+    return p1c, p2
 
 
 if __name__=="__main__":
--- day18_lagoon/test_lagoon.py
+++ day18_lagoon/test_lagoon.py
@@ -1,14 +1,13 @@
 import pytest
 from lagoon import read, solve_part1, solve_part2, main
 
-data = read("day18_lagoon/example")
+data = read("day18_lagoon/example", "DR")
 
 def test_part1():
     assert solve_part1(data) == 62
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2():
-    assert solve_part2()
+    assert solve_part2(data) == 952408144115
 
 @pytest.mark.real
 def test_main():
